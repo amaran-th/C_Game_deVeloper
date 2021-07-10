a31f934436cf436ae8284d1dcf35b8528be95b35
lcqff <afteryear199@naver.com>
2021-07-09 22:39:51 -0700

    not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
+//const { Game } = require("phaser");
+
 var game;
  
 // 게임 인스턴스를 만든다.
-game = new Phaser.Game(600, 450, Phaser.AUTO, '');
- 
+//game = new Phaser.Game(600, 450, Phaser.AUTO, '');
+
+game = new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 600,
+    height: 600,
+    physics: {
+        default: 'arcade',
+    },
+    backgroundColor: 0x9cbbd8,
+    Scene: [TestScene] //class name?
+  });
+
 //  게임 인스턴스를 초기화 한 후, 상태를 추가한다.
 game.state.add('Menu', Menu);
-game.state.add('Game', Game);
+game.state.add('Game', TestScene); 
 
 game.state.start('Menu');
 
---
+++ assets/js/test2.js
@@ -0,0 +1,9 @@
+var add = function(x,y) {
+    return x+y;
+};
+
+var show_func = function(add) {
+    console.log(add);
+};
+
+show_func();
