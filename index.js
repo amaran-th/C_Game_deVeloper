1be0c14282e753b968b390d97ba3cb45556fab82
pipisebastian <yrt7998@pusan.ac.kr>
2021-07-11 23:51:42 +0900

    init snake_game

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
+const express = require('express'); 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4200); // 서버 포트 설정.  
+
+const morgan = require('morgan')
+app.use(morgan('dev'))
+//morgan 미들웨어 : GET /assets/images/menu.png 304 0.916 ms - - 
+
+
+app.use(express.static(__dirname + '/'))
+//정적 파일 제공 미들웨어asa
+//npm으로 설치할 필요 x. 이미 내장
+//서버에서 a.jpg 찾음 => ./pubic폴더에서 찾음
+
+
+app.engine('html', require('ejs').renderFile);
 app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set("view engine", "html");
+//render(html)할려고 한 코드들..
 
-var mainRouter = require("./router/main");
-var choiceRouter = require("./router/choice");
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.get('/test',function(req,res){
+    console.log('tets~s...');
+    res.render('test')
+});//이건 걍 웹페이지 전환 코드 
 
-app.use('/',mainRouter);
-app.use('/choice',choiceRouter);
+//id 보내는 코드 .html에서 '보내기' => /send_email POST
+app.use(express.json()); //req로 데이터 받을때. 
+app.use(express.urlencoded({extended: false})); //마찬가지
 
-app.listen(app.get('port'),function(){
+app.post('/send_code', function(req,res){
+    console.log("code :", req.body.code);   //post로 받은건 req.body에 저장!                                                                 
+    res.send("<h1>WELCOME<h1>");
+  });
+///////////////////
+
+
+app.get('/',function(req,res){
+    console.log('[main.js] Main page loading...');
+    res.render('index')
+
+});
+app.listen(app.get('port'),function(){  //서버 연결 
     console.log('[Listening] localhost @',app.get('port'));
 });
--- router/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/',async function(req,res){
-    console.log('[main.js] Main page loading...');
-    res.render('main');
-});
-
-module.exports = router;
