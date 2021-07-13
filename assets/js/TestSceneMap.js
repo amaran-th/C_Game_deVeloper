0d487d16b5d086e611d85ec26d80f5eb834e897f
lcqff <afteryear199@naver.com>
2021-07-13 05:47:36 -0700

    map but no collision

--- assets/js/TestSceneMap.js
+++ assets/js/TestSceneMap.js
@@ -5,25 +5,30 @@ class TestSceneMap extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("tiles", "./assets/image/testSceneMap.png");
+    this.load.image("tiles", "./assets/images/testSceneMap.png");
     this.load.tilemapTiledJSON("map", "./assets/testSceneMap.json");
   }
 
   create() {
     const map = this.make.tilemap({ key: "map" });
-  
-    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
-    // Phaser's cache (i.e. the name you used in preload)
-    const tileset = map.addTilesetImage("testSceneMap", "tiles"); //name of tileset , source  //INVAILD TILESET IMAGE
+    const tileset = map.addTilesetImage("testSceneMap", "tiles"); //name of tileset(which is same as Png tileset) , source  //INVAILD TILESET IMAGE
   
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const belowLayer = map.createStaticLayer("sign", tileset, 0, 0);
-    const worldLayer = map.createStaticLayer("ground", tileset, 0, 0);
-    //const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+    const worldLayer = map.createLayer("ground", tileset, 0, 0);
+    const aboveLayer = map.createLayer( "sign", tileset, 0, 0);
+    //const belowLayer = map.createLayer("Above Player", tileset, 0, 0);
 
     worldLayer.setCollisionByProperty({ collides: true });
 
-    console.log("build testSceneMap");
+    
+    const debugGraphics = this.add.graphics().setAlpha(0,75);
+    worldLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243,134,48,255),
+      faceColor: new Phaser.Display.Color(40,39,37,255)
+    });
+    
+        console.log("build testSceneMap");
   }
 
 }
