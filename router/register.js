e7022157af15b54a0611f86695517dcf0ebc09b0
pipisebastian <yrt7998@pusan.ac.kr>
2021-07-24 05:43:42 +0900

    id 중복체크 기능 추가

--- router/register.js
+++ router/register.js
@@ -13,53 +13,49 @@ router.route('/')
     })
     .post(async (req, res, next) => {
         try {
-            console.log("=======Try to add user=======");
+            console.log("[Try to add user]");
+            /////id 중복 확인
+            const idcheck = await User.findOne({
+                where: {
+                    id: req.body.id
+                }
+            }, { raw: true });
 
-            var nick = req.body.nick;
-            var id = req.body.id;
-            var pw = req.body.pw;
+            if (idcheck != null) {
+                console.log("[ID Dup]");//중복
+                //alert("중복입니다 다시 입력하세여"); 서버에서는 alert 안됨 방법 없나??
+                res.render("register");
+            } else {
+                console.log("[ID OK]");
+           
+                var nick = req.body.nick;
+                var id = req.body.id;
+                var pw = req.body.pw;
 
-            var salt = crypto.randomBytes(64).toString("base64");//보안
+                var salt = crypto.randomBytes(64).toString("base64");//보안
             
-            var hashPw = crypto //보안
-                .pbkdf2Sync(pw, salt, 100, 64, "sha512")
-                .toString("base64");
+                var hashPw = crypto //보안
+                    .pbkdf2Sync(pw, salt, 100, 64, "sha512")
+                    .toString("base64");
 
-            await User.create({
-                nickname: nick,
-                id: id,
-                pw: hashPw, // 그냥 pw가 아님!!
-                salt: salt,
-            });
+                await User.create({
+                    nickname: nick,
+                    id: id,
+                    pw: hashPw, // 그냥 pw가 아님!!
+                    salt: salt,
+                });
 
-            console.log("=======Success to INSERT new user=======");
-            res.render("main") //회원가입 되면 main페이지로.
+                console.log("=======Success to INSERT new user=======");
+                res.render("index") //회원가입 되면 기본페이지로.
+            }
 
         } catch (err) {
             console.log(err);
             console.log("======Failed to INSERT new user======");
-            res.render("register")
-           
+            res.render("register") //중복말고 다른 에러가 뜰시
+            
         }
     })
 
-router.route('/idcheck')
-    .post(async (req, res, next) => {
-        console.log("[POST] ID Check");
-        console.log(req.body);
-        const idcheck = await User.findOne({
-            where: {
-                username: req.body.id
-            }
-        }, { raw: true });
-
-        if (idcheck != null) {
-            res.json({ duplicate: 1 });
-            console.log("ID Dup");
-        } else {
-            res.json({ duplicate: 0 });
-            console.log("ID OK");
-        }
-    })
 
 module.exports = router;
