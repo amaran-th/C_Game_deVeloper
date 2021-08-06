dbbcd9794bc5c559b6bb6113ff5602f68be89daf
llddang <bodyness@naver.com>
2021-08-06 19:24:55 +0900

    글자코드 먹고나서 인벤확인하게 느낌표 달아주기

--- assets/js/Inventory.js
+++ assets/js/Inventory.js
@@ -10,7 +10,7 @@ export default class inventory {
         this.inventory = scene.add.graphics();
         this.inventory.lineStyle(3, 0xFFB569, 1);
         this.inventoryHandle = this.inventory.fillRoundedRect(0, 0, 150, 50, 5).strokeRoundedRect(0, 0, 150, 50, 5);
-        this.inventoryBody = this.inventory.fillRoundedRect(0, 50, 1100, 200, 20).strokeRoundedRect(0, 50, 1100, 200, 20);
+        this.inventoryBody = this.inventory.fillRoundedRect(0, 50, 1094, 173, 20).strokeRoundedRect(0, 50, 1094, 173, 20);
         this.inventory.fillStyle(0xFCE5CD, 1);
         this.invenText = scene.add.text(5,10,'Inventory',{
             fontSize : '25px',
@@ -19,14 +19,14 @@ export default class inventory {
         }).setOrigin(0,0);
 
         //인벤창을 모두 하나의 오브젝트로 묶기
-        this.inven = scene.add.container(0,550, [this.inventoryHandle,this.inventoryBody, this.invenText]);
+        this.inven = scene.add.container(3,550, [this.inventoryHandle,this.inventoryBody, this.invenText]);
         this.inven.setSize(200, 100)
 
         this.inven.setInteractive();
 
 
         this.item = new Array(); //저장되는 아이템
-        this.invenIn = true; //인벤토리 창이 내려가있는지 올라가있는지
+        this.invenIn = false; //인벤토리 창이 내려가있는지 올라가있는지
 
         console.log('인덱스:', this.item.length);
 
@@ -42,13 +42,19 @@ export default class inventory {
         //console.log(this.invenIn); 
 
         this.inven.once('pointerdown', () => {
-            if(this.invenIn) { this.inven.y = 375;} 
+            if(this.invenIn) { 
+                this.inven.y = 375;
+                if(this.exclamationIsReal){
+                    this.exclamation.destroy();
+                    this.exclamationIsReal = false;
+                }
+            } 
             else { this.inven.y = 550;}
             //console.log('clicked');
             this.invenIn = !this.invenIn;
         })
 
-
+        this.exclamationIsReal = false;
     }
 
     invenSave(scene, itemName) {
@@ -61,6 +67,10 @@ export default class inventory {
              fill: "#f9cb9c" 
             });
         console.log('글자 위치:', this.newItem.y);
+        if(!this.invenIn) { 
+            this.exclamation = scene.add.text(140, 540,'!', { font: "30px Arial Black", fill: "#ff0000" }); 
+            this.exclamationIsReal = true;
+        }
         this.newItem.setInteractive();
         scene.input.setDraggable(this.newItem);
         this.inven.add(this.newItem);
