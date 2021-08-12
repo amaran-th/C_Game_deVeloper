56ee6a538e25e57029877ab3dc5b589c5ece5c5c
lcqff <afteryear199@naver.com>
2021-08-12 18:57:45 +0900

    stage1 퀴즈 정답 유무 확인 기능은 아직 안넣음

--- assets/js/stages/Quiz.js
+++ assets/js/stages/Quiz.js
@@ -29,28 +29,40 @@ export default class Quiz extends Phaser.Scene {
         graphics = this.add.graphics();
         path = { t: 0, vec: new Phaser.Math.Vector2() };
 
-        curve[0] = new Phaser.Curves.Line([ 300, 300, 300, 300 ]); //선 시작위치, 끝나는 위치 //int
-        curve[1] = new Phaser.Curves.Line([ 300, 400, 300, 400 ]); //double
-        curve[2] = new Phaser.Curves.Line([ 300, 500, 300, 500 ]); //char
+        curve[0] = new Phaser.Curves.Line([ 300, 170, 300, 170 ]); //선 시작위치, 끝나는 위치 //int
+        curve[1] = new Phaser.Curves.Line([ 300, 270, 300, 270 ]); //double
+        curve[2] = new Phaser.Curves.Line([ 300, 370, 300, 370 ]); //char
+        curve[3] = new Phaser.Curves.Line([ 300, 470, 300, 470 ]); //char
     }
 
     start_quiz() {
-        
-        var graphics_zone = this.add.graphics();
+        //글자...
+        var quiz = ['3.14', '-64', '36', '가'];
+        var answer= [ 'Int', 'Double', 'Char'];
+
+
+        //드랍존
         for(var i=0; i<= 3; i++) {
+            this.add.text(200, 150 + 100*i , quiz[i], { font: "30px Arial Black", fill: "#fff" });
+            var problem = this.add.circle(curve[i].p0.x,curve[i].p0.y,10,10,0xefc53f).setInteractive();
+            problem.setData('vector', curve[i].p0);
+            this.input.setDraggable([ problem ]);
+           
+        }
+
+        //마우스로 드래그 하는 원
+        var graphics_zone = this.add.graphics();
+        for(var i=0; i<= 2; i++) {
+            this.add.text(830, 230 + 100*i , answer[i], { font: "30px Arial Black", fill: "#fff" });
             graphics_zone.lineStyle(2, 0xffff00);
-            var zone = this.add.zone(800, 200 + 100*i).setCircleDropZone(10);
+            var zone = this.add.zone(800, 250 + 100*i).setCircleDropZone(10);
             graphics_zone.strokeCircle(zone.x, zone.y, zone.input.hitArea.radius);
         }
 
-        for(var i=0; i< 3; i++) {
-        var problem0 = this.add.circle(curve[i].p0.x,curve[i].p0.y,10,10,0xefc53f).setInteractive();
-        problem0.setData('vector', curve[i].p0);
-        this.input.setDraggable([ problem0 ]);
-        }
 
         this.input.on('dragstart', function (pointer, gameObject) {
             //gameObject.setTint(0xf9cb9c);
+            console.log(gameObject);
         });
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
             gameObject.x = dragX;
@@ -72,6 +84,7 @@ export default class Quiz extends Phaser.Scene {
     update(){
         graphics.clear();
         graphics.lineStyle(2, 0xffffff, 1);
+
         for(var i = 0; i<curve.length; i++ ){
             curve[i].draw(graphics);
             curve[i].getPoint(path.t, path.vec);
