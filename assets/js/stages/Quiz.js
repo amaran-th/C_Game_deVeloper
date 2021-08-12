c3d70541a3ebb77f6702c7b1406c89951f426942
lcqff <afteryear199@naver.com>
2021-08-12 18:29:30 +0900

    선 만드는 거 끝냄

--- assets/js/stages/Quiz.js
+++ assets/js/stages/Quiz.js
@@ -1,4 +1,4 @@
-var curve;
+var curve = [];
 var graphics;
 var path;
 
@@ -14,6 +14,7 @@ export default class Quiz extends Phaser.Scene {
         console.log('퀴즈 시작!');
         //this.inventory.fillRoundedRect(0, 50, 1094, 173, 20).strokeRoundedRect(0, 50, 1094, 173, 20);
         var screen = this.add.rectangle(150, 100, 10, 10, 0x9966ff).setOrigin(0,0);
+        //dropzone
         this.tweens.add({
             targets: screen,
             scaleX: 80,
@@ -27,16 +28,29 @@ export default class Quiz extends Phaser.Scene {
 
         graphics = this.add.graphics();
         path = { t: 0, vec: new Phaser.Math.Vector2() };
-        curve = new Phaser.Curves.Line([ 250, 200, 250, 200 ]);
+
+        curve[0] = new Phaser.Curves.Line([ 300, 300, 300, 300 ]); //선 시작위치, 끝나는 위치 //int
+        curve[1] = new Phaser.Curves.Line([ 300, 400, 300, 400 ]); //double
+        curve[2] = new Phaser.Curves.Line([ 300, 500, 300, 500 ]); //char
     }
 
     start_quiz() {
-        var problem0 = this.add.circle(curve.p0.x,curve.p0.y,10,10,0xefc53f).setInteractive();
-        problem0.setData('vector', curve.p0);
+        
+        var graphics_zone = this.add.graphics();
+        for(var i=0; i<= 3; i++) {
+            graphics_zone.lineStyle(2, 0xffff00);
+            var zone = this.add.zone(800, 200 + 100*i).setCircleDropZone(10);
+            graphics_zone.strokeCircle(zone.x, zone.y, zone.input.hitArea.radius);
+        }
+
+        for(var i=0; i< 3; i++) {
+        var problem0 = this.add.circle(curve[i].p0.x,curve[i].p0.y,10,10,0xefc53f).setInteractive();
+        problem0.setData('vector', curve[i].p0);
         this.input.setDraggable([ problem0 ]);
+        }
 
         this.input.on('dragstart', function (pointer, gameObject) {
-            gameObject.tint = 0x21f7ef;
+            //gameObject.setTint(0xf9cb9c);
         });
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
             gameObject.x = dragX;
@@ -44,15 +58,24 @@ export default class Quiz extends Phaser.Scene {
             gameObject.data.get('vector').set(dragX, dragY);
         });
 
-        this.input.on('dragend', function (pointer, gameObject) {
+        this.input.on('dragend', function (pointer, gameObject,dropped) {
             //gameObject.clearTint();
+            if (!dropped) //이거 없으면 마우스 놓은 자리에 유지됨
+            {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+                gameObject.data.get('vector').set(gameObject.x, gameObject.y); //선도 같이 제자리도 돌아가도록
+            }
         });
     }
 
     update(){
         graphics.clear();
         graphics.lineStyle(2, 0xffffff, 1);
-        curve.draw(graphics);
-        curve.getPoint(path.t, path.vec);
+        for(var i = 0; i<curve.length; i++ ){
+            curve[i].draw(graphics);
+            curve[i].getPoint(path.t, path.vec);
+        }
+
     }
 }
