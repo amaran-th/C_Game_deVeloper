f11a0faaa2ab2fbf35376198355eb3b7c9945426
SongSeyeon <songsy405@pusan.ac.kr>
2021-08-15 07:19:09 -0700

    숫자키 눌렀을 때 맵 이동하는 거 추가

--- assets/js/stages/SecondStage.js
+++ assets/js/stages/SecondStage.js
@@ -25,6 +25,9 @@ export default class SecondStage extends Phaser.Scene {
         this.keyX = this.input.keyboard.addKey('X');
         this.key1 = this.input.keyboard.addKey('ONE');
         this.key3 = this.input.keyboard.addKey('THREE');
+        this.key4 = this.input.keyboard.addKey('FOUR');
+        this.key5 = this.input.keyboard.addKey('FIVE');
+        this.key6 = this.input.keyboard.addKey('SIX');
 
         this.anims.create({
             key: "fire",
@@ -180,6 +183,21 @@ export default class SecondStage extends Phaser.Scene {
             console.log('맵이동');
             this.scene.sleep('second_stage'); //방으로 돌아왔을 때 플레이어가 문 앞에 있도록 stop 말고 sleep (이전 위치 기억)
             this.scene.run("third_stage");
+        }
+        if(this.key4.isDown) {
+            console.log('맵이동');
+            this.scene.sleep('second_stage'); //방으로 돌아왔을 때 플레이어가 문 앞에 있도록 stop 말고 sleep (이전 위치 기억)
+            this.scene.run("fourth_stage");
+        }
+        if(this.key5.isDown) {
+            console.log('맵이동');
+            this.scene.sleep('second_stage'); //방으로 돌아왔을 때 플레이어가 문 앞에 있도록 stop 말고 sleep (이전 위치 기억)
+            this.scene.run("fifth_stage");
+        }
+        if(this.key6.isDown) {
+            console.log('맵이동');
+            this.scene.sleep('second_stage'); //방으로 돌아왔을 때 플레이어가 문 앞에 있도록 stop 말고 sleep (이전 위치 기억)
+            this.scene.run("sixth_stage");
         }
 
     }
