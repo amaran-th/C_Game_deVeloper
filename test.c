f19cc4264e057591fa5ca19322780f28baa2eb62
lcqff <afteryear199@naver.com>
2021-09-05 16:21:57 +0900

    wasd 이동

--- test.c
+++ test.c
@@ -1,4 +1,9 @@
-NaN
-int main(){ 
-     undefined("undefined"); 
- }
+#include <stdio.h>
+int main(){
+   int bread=1;
+   int i;
+   undefined(int i=0; iundefined24; i++){
+      bread=bread+1;
+   }
+   printf("bread=%d",bread);
+}
