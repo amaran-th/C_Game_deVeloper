0c8301914838bd0ef9b688fc03644846a769281a
kej <publdaze@naver.com>
2021-09-07 04:05:27 +0900

    codepiece move with play (not reflect codepiece text only but reflect codepiece text and dropzone position)

--- assets/js/CodePiece.js
+++ assets/js/CodePiece.js
@@ -21,10 +21,36 @@ class CodePiece extends Phaser.GameObjects.Text {
     update(scene) {
         if (scene.worldView.x != this.preworldview_x) { // 코드 조각 플레이어 따라 이동
             for (var i = 0; i < this.codepiece_textObject_arr.length; i++) {
-                //console.log(this.codepiece_textObject_arr[i].x);
-                switch (this.codepiece_textObject_arr[i]._text) { // 드랍존에 들어가지 않은 코드 조각은 그냥 플레이어 따라가고
+                //console.log(scene.code_zone_1, this.codepiece_textObject_arr[i]._text)
+                //console.log(this.codepiece_textObject_arr[0].x, scene.draganddrop_1.x - (scene.draganddrop_1.width / 2) + 5, scene.worldView.x + scene.dropzone1_x - (scene.draganddrop_1.width / 2) + 5);
+                // 조건 설명
+                // (scene.code_zone_1 == this.codepiece_textObject_arr[i]._text) : 태그조각 문자열과 해당 드랍존에 들어간 문자열이 같을 때 (어떤 드랍존에 위치해있는 지 알아야지 그 드랍존에 맞춰서 위치 이동 가능)
+                // (this.codepiece_textObject_arr[i].x == scene.draganddrop_1.x - (scene.draganddrop_1.width / 2) + 5) : 같은 문자열일 경우 위의 조건에서 걸러지지 않음 -> 드랍존의 이전 위치와 비교해서 그 위치에 태그조각이 있을 경우를 추가 조건으로 줌
+                if ((scene.code_zone_1 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_1.x - (scene.draganddrop_1.width / 2) + 5)) { 
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone1_x - (scene.draganddrop_1.width / 2) + 5; // 현재 드랍존의 위치를 태그 조각에 반영 함.
+                }
+                if ((scene.code_zone_2 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_2.x - (scene.draganddrop_2.width / 2) + 5)) { // 같은 태그라도 플레이어 따라 다 이동해야하므로 elseif 말고 if로 함
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone2_x - (scene.draganddrop_2.width / 2) + 5;
+                }
+                if ((scene.code_zone_3 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_3.x - (scene.draganddrop_3.width / 2) + 5)) { 
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone3_x - (scene.draganddrop_3.width / 2) + 5;
+                }
+                if ((scene.code_zone_4 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_4.x - (scene.draganddrop_4.width / 2) + 5)) { 
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone4_x - (scene.draganddrop_4.width / 2) + 5;
+                }
+                if ((scene.code_zone_5 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_5.x - (scene.draganddrop_5.width / 2) + 5)) { 
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone5_x - (scene.draganddrop_5.width / 2) + 5;
+                }
+                if ((scene.code_zone_6 == this.codepiece_textObject_arr[i]._text) && (this.codepiece_textObject_arr[i].x == scene.draganddrop_6.x - (scene.draganddrop_6.width / 2) + 5)) { 
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone6_x - (scene.draganddrop_6.width / 2) + 5;
+                }
+                // 수 추가해야 함
+                if (this.codepiece_textObject_arr[i].x < scene.worldView.x + 160) { // 인벤창 우측 끝보다 왼쪽에 있을 때 코드조각이 인벤창 안에 있다고 판단함 (드랍존에 안 들어가 있는 코드조각 플레이어 따라 이동하도록 하기)
+                    this.codepiece_textObject_arr[i].x = scene.worldView.x + 15;
+                }
+                /*switch (this.codepiece_textObject_arr[i]._text) { // 드랍존에 들어가지 않은 코드 조각은 그냥 플레이어 따라가고
                     case scene.code_zone_1:         // 드랍존에 들어간 코드조각은 어는 드랍존인 지 구분하여 해당 드랍존 위치에 맞게 플레이어를 따라가도록 함
-                        this.codepiece_textObject_arr[i].x = scene.draganddrop_1.x - (scene.draganddrop_1.width / 2) + 5;
+                        this.codepiece_textObject_arr[i].x = scene.worldView.x + scene.dropzone1_x - (scene.draganddrop_1.width / 2) + 5;
                         break;
                     case scene.code_zone_2:
                         this.codepiece_textObject_arr[i].x = scene.draganddrop_2.x - (scene.draganddrop_2.width / 2) + 5;
@@ -67,7 +93,7 @@ class CodePiece extends Phaser.GameObjects.Text {
                         break;
                     default:
                         this.codepiece_textObject_arr[i].x = scene.worldView.x + 15;
-                }
+                }*/
             }
             this.preworldview_x = scene.worldView.x;
         }
